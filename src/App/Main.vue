0915bd8ac97ed94126a8a854d2bd1c6bb3f442b9
roney ruiz <roneyruizrojas@gmail.com>
2025-08-08 14:46:51 -0600

    chore: remove default files

---
+++ src/App/Main.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"/>
+
+<template>
+  <p>Hello World!</p>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@App": "/src/App",
+      "@Routes": "/src/Routes",
+    },
+  },
+});
