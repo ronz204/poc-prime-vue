766fe338da3fc3a0304a28d8ed7f6741e116d8ab
roney ruiz <roneyruizrojas@gmail.com>
2025-08-08 21:50:34 -0600

    deps: install and config pinia state management

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import "./style.css";
 import { Theme } from "./theme";
 import { createApp } from "vue";
 import Main from "@App/Main.vue";
+import { createPinia } from "pinia";
 import PrimeVue from "primevue/config"
 
 const app = createApp(Main);
+
+app.use(createPinia());
 app.use(PrimeVue, { theme: Theme });
 
 app.mount("#app");
