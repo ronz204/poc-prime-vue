7e2a0d2f64669902c36b183a7cf04b4e902d8119
roney ruiz <roneyruizrojas@gmail.com>
2025-08-13 22:02:30 -0600

    feat: create rotuer and add to app

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import "./style.css";
 import { Theme } from "./theme";
 import { createApp } from "vue";
 import Main from "@App/Main.vue";
+import { Router } from "./router";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config"
 
 const app = createApp(Main);
 
+app.use(Router);
 app.use(createPinia());
 app.use(PrimeVue, { theme: Theme });
 
