ecb22997000cb1e2e3ded782eb4a24c05f804a01
roney ruiz <roneyruizrojas@gmail.com>
2025-08-25 11:21:18 -0600

    feat(client): setup pinia store

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,10 +2,12 @@ import "./style.css";
 import Main from "@App/Main.vue";
 
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import { Router } from "./router";
 
 const app = createApp(Main);
 
 app.use(Router);
+app.use(createPinia());
 
 app.mount("#app");
