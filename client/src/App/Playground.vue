1df9c345411c133c0d9be4a4c43c9b24fb3f4cae
roney ruiz <roneyruizrojas@gmail.com>
2025-08-25 13:17:14 -0600

    feat(client): create a little example of prime vue

--- client/src/App/Playground.vue
+++ client/src/App/Playground.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
-/* import Button from "primevue/button";
+import Button from "primevue/button";
 
 const ClickingHandler = (): void => {
   console.log("you are clicking the button");
-}; */
+};
 </script>
 
 <template>
   <div class="p-10">
-    <!-- <Button label="Clicking" @click="ClickingHandler" /> -->
+    <Button label="Clicking" @click="ClickingHandler" />
   </div>
 </template>
