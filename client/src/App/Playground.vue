5153917d69dd5a7e707ce2c754487262b335a8bc
roney ruiz <roneyruizrojas@gmail.com>
2025-08-25 11:14:45 -0600

    feat(client): create playground and router

---
+++ client/src/App/Playground.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+/* import Button from "primevue/button";
+
+const ClickingHandler = (): void => {
+  console.log("you are clicking the button");
+}; */
+</script>
+
+<template>
+  <div class="p-10">
+    <!-- <Button label="Clicking" @click="ClickingHandler" /> -->
+  </div>
+</template>
---
+++ client/src/main.ts
@@ -0,0 +1,11 @@
+import "./style.css";
+import Main from "@App/Main.vue";
+
+import { createApp } from "vue";
+import { Router } from "./router";
+
+const app = createApp(Main);
+
+app.use(Router);
+
+app.mount("#app");
